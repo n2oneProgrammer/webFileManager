20210e0d713a43a5baf4b3a2b66394eefc7037ea
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-03-27 20:59:55 +0200

    change url

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,55 +1,35 @@
 export class Api {
     static async getFiles(src) {
-        let url = "";
-        if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000/";
-        }
-        let data = await fetch(url + "get?pos=" + src);
+        let data = await fetch(document.URL + "/get?pos=" + src);
         let json = await data.json();
         console.log(json);
         return json;
     }
 
     static async upload(file, path) {
-        let url = "";
-        if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000/";
-        }
         let body = new FormData();
         body.append("file", file);
         body.append("path", path);
 
-        await fetch(`${url}upload`, {
+        await fetch(`${document.URL}/upload`, {
             method: "POST", body: body
         });
     }
 
     static async rename(old, newName) {
-        let url = "";
-        if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000/";
-        }
-        await fetch(`${url}rename?old=${old}&newname=${newName}`, {
+        await fetch(`${document.URL}/rename?old=${old}&newname=${newName}`, {
             method: "POST"
         });
     }
 
     static async remove(path) {
-        let url = "";
-        if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000/";
-        }
-        await fetch(`${url}delete?path=${path}`, {
+        await fetch(`${document.URL}delete?path=${path}`, {
             method: "POST"
         });
     }
 
     static async download(path, name) {
-        let url = "";
-        if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000/";
-        }
-        let response = await fetch(`${url}download?path=${path}`);
+        let response = await fetch(`${document.URL}download?path=${path}`);
         let blob = await response.blob();
         const link = document.createElement("a");
         link.href = URL.createObjectURL(blob);
