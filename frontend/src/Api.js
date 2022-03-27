01e049a2a8584419c091f7fccafdea973ea2c6b5
n2oneProgrammer <kubawojtasik70@gmail.com>
2022-03-27 16:16:17 +0200

    some changes

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -2,9 +2,9 @@ export class Api {
     static async getFiles(src) {
         let url = "";
         if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000";
+            url = "http://localhost:8000/";
         }
-        let data = await fetch(url + "/get?pos=" + src);
+        let data = await fetch(url + "get?pos=" + src);
         let json = await data.json();
         console.log(json);
         return json;
@@ -13,13 +13,13 @@ export class Api {
     static async upload(file, path) {
         let url = "";
         if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000";
+            url = "http://localhost:8000/";
         }
         let body = new FormData();
         body.append("file", file);
         body.append("path", path);
 
-        await fetch(`${url}/upload`, {
+        await fetch(`${url}upload`, {
             method: "POST", body: body
         });
     }
@@ -27,9 +27,9 @@ export class Api {
     static async rename(old, newName) {
         let url = "";
         if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000";
+            url = "http://localhost:8000/";
         }
-        await fetch(`${url}/rename?old=${old}&newname=${newName}`, {
+        await fetch(`${url}rename?old=${old}&newname=${newName}`, {
             method: "POST"
         });
     }
@@ -37,9 +37,9 @@ export class Api {
     static async remove(path) {
         let url = "";
         if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000";
+            url = "http://localhost:8000/";
         }
-        await fetch(`${url}/delete?path=${path}`, {
+        await fetch(`${url}delete?path=${path}`, {
             method: "POST"
         });
     }
@@ -47,9 +47,9 @@ export class Api {
     static async download(path, name) {
         let url = "";
         if (process.env.NODE_ENV === "development") {
-            url = "http://localhost:8000";
+            url = "http://localhost:8000/";
         }
-        let response = await fetch(`${url}/download?path=${path}`);
+        let response = await fetch(`${url}download?path=${path}`);
         let blob = await response.blob();
         const link = document.createElement("a");
         link.href = URL.createObjectURL(blob);
